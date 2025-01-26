b4843988b78e53ccebc4045086f68dcc77801001
Aditya Vaish <adityavaish846@gmail.com>
2025-01-26 18:19:46 +0530

    Snake File added

--- Snake.cpp
+++ Snake.cpp
@@ -1,7 +1,17 @@
-//The-Great-Devourer Snake Game made in C++
+//The-Great-Devourer Snake Game
+//Made by: Vaishcodescape and sam5506
+#include <bits/stdc++.h>
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 using namespace std;
+void printsnake()
+{
+
+}
 int main()
 {
+    printsnake();
     return 0;
 }
