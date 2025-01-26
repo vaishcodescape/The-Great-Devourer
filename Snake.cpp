cc9f52b895421a222b90a86f417c62c6599ae678
Aditya Vaish <adityavaish846@gmail.com>
2025-01-27 00:08:08 +0530

    Changes made to this repo

--- Snake.cpp
+++ Snake.cpp
@@ -4,10 +4,17 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-
+#ifdef _WIN32
+#include <windows.h>
+#endif
 using namespace std;
 void printsnake()
 {
+     cout << " TTTTT  H   H  EEEEE      GGG   RRRR    EEEEE   AAAAA  TTTTT      DDDD   EEEEE   V     V  OOO   U   U  RRRR    EEEEE   RRRR   \n";
+    cout << "   T    H   H  E          G      R   R   E       A   A    T        D   D  E        V   V  O   O  U   U  R   R   E       R   R  \n";
+    cout << "   T    HHHHH  EEEE       G  GG  RRRR    EEEE    AAAAA    T        D   D  EEEE       V    O   O  U   U  RRRR    EEEE    RRRR   \n";
+    cout << "   T    H   H  E          G   G  R  R    E       A   A    T        D   D  E        V   V  O   O  U   U  R  R    E       R  R   \n";
+    cout << "   T    H   H  EEEEE      GGG    R  R    EEEEE   A   A    T        DDDD   EEEEE      V     OOO    UUU   R   R   EEEEE   R   R  \n";
 
 }
 int main()
